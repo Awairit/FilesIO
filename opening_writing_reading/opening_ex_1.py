a8f414f42cc58427fec518386b56c4ead5e22c5e
Mohammad Irshad <awairit2k20@gmail.com>
2025-10-24 10:41:52 +0530

    Conceptual Theory

--- opening_writing_reading/opening_ex_1.py
+++ opening_writing_reading/opening_ex_1.py
@@ -1,1 +1,4 @@
-#File Modes
+#File Modes r, w, a, r+, w+, a+, x, x+
+#open files by two methods
+# 1. 'open("fileName", "fileMode")' Need to close opened file by filePointer.close(), --> syntax: variable_name=open("fileName", "fileMode")
+# 2. 'with open("fileName", "fileMode") as :' no need to close PVM will close as lines of code get out from indentation block, --> syntax:  with open("fileName", "fileMode") as variable_name:
