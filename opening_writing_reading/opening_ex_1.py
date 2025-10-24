a2b66cfbc59621b3d0398e8340d520b64ed52dde
Mohammad Irshad <awairit2k20@gmail.com>
2025-10-24 10:46:39 +0530

    FileNotFoundError: if file is not present we cant read it thats why it gives FileNotFoundError

--- opening_writing_reading/opening_ex_1.py
+++ opening_writing_reading/opening_ex_1.py
@@ -2,3 +2,5 @@
 #open files by two methods
 # 1. 'open("fileName", "fileMode")' Need to close opened file by filePointer.close(), --> syntax: variable_name=open("fileName", "fileMode")
 # 2. 'with open("fileName", "fileMode") as :' no need to close PVM will close as lines of code get out from indentation block, --> syntax:  with open("fileName", "fileMode") as variable_name:
+
+fp = open("student.txt","r") #if file is not present we cant read it that's why it gives "FileNotFoundError"
