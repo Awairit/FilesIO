0338a832548928d2257092d4ce59a9e1fa0fc84e
Mohammad Irshad <awairit2k20@gmail.com>
2025-10-24 12:15:51 +0530

    Concepts and Syntax

--- writing/writing_ex_1.py
+++ writing/writing_ex_1.py
@@ -1,1 +1,5 @@
 # Writing the Data to the File
+# To write the Data to the File, we have Two Pre-Defined Functions Present in File Pointer Object. They are
+#     1. write()      --> Syntax:   FilePointerObj.write(str-data)                  ,If we have non-str Data then we convert the non-str data into str type data by using str()
+#     2. writelines() --> Syntax:   FilePointerObj.writelines(Iterable-Object Data) ,If we have non-str Data then we convert the non-str data into str type data by using str()
+
