d880b7060ccc2ed3d68d16b892a0c1adef56f15d
Mohammad Irshad <awairit2k20@gmail.com>
2025-10-24 10:51:49 +0530

    we have FileNotFoundError include try except else block

--- opening_writing_reading/opening_ex_1.py
+++ opening_writing_reading/opening_ex_1.py
@@ -3,4 +3,9 @@
 # 1. 'open("fileName", "fileMode")' Need to close opened file by filePointer.close(), --> syntax: variable_name=open("fileName", "fileMode")
 # 2. 'with open("fileName", "fileMode") as :' no need to close PVM will close as lines of code get out from indentation block, --> syntax:  with open("fileName", "fileMode") as variable_name:
 
-fp = open("student.txt","r") #if file is not present we cant read it that's why it gives "FileNotFoundError"
+try:
+    fp = open("student.txt","r") #if file is not present we cant read it that's why it gives "FileNotFoundError"
+except FileNotFoundError:
+    print("File not found, please check the path or File Does not Exist")
+else:
+    print("File opened successfully") #if 'try:' block works 'else' block works
